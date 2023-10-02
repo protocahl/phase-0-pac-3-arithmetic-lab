fb27e915891805750449bdede7ccd473ac192a81
Armani Simmons <fibonaccy@protonmail.com>
2023-10-02 12:39:07 -0400

    complete

---
+++ index.js
@@ -0,0 +1,40 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Function to subtract two numbers
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Function to multiply two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Function to divide two numbers
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Function to increment a number by 1
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Function to decrement a number by 1
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Function to parse a string into an integer (base 10)
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  // Function to parse a string into a floating-point number (float)
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
